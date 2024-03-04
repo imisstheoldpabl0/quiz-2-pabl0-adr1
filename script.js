5810ebf05f0218259a22da77ac2cffb6eba60195
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-03-04 17:21:24 +0100

    add/changes + readme

---
+++ script.js
@@ -0,0 +1,8 @@
+`<fieldset> // PINTAR ESTO CON UNA NUEVA PREGUNTA Y RESPUESTAS CADA VEZ QUE SE MARQUE UNA OPCIÓN DE LA PREGUNTA ANTERIOR
+    <legend>
+        <article>
+            <label for=""></label> // OPCION 1 CAPTURAR RESPUESTA Y GUARDAR EN ARRAY PARA CONTRASTAR Y REPINTAR PREGUNTA + RESPUESTAS
+            <input type="text">
+        </article>
+    </legend>
+</fieldset>` 
