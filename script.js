f2ad014263d62634a3886ec70111a2fa8a383050
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-03-12 17:26:28 +0100

    single page application/formulario guarda a firebase/grafica se muestra al terminar el quiz/falta enseñar al usuario en todo momento si esta loggeado o no

--- script.js
+++ script.js
@@ -1,3 +1,167 @@
+
+/* EL BUEN SCRIPT */
+
+function welcome() {
+    document.querySelector("#quiz").innerHTML = "";
+    document.querySelector("#welcome").innerHTML = `
+        <section>
+            <h2 id="userLog"></h2>
+            <div id="forms-container">
+                <div>
+                    <h3>Formulario de Registro</h3>
+                    <form id="form1" action="#" method="POST">
+                        <div>
+                            <label for="email">Email:</label><br>
+                            <input type="email" id="email" name="email" placeholder="Introduce email..."><br>
+                            <label for="pass">Password:</label><br>
+                            <input type="password" id="pass" name="pass" placeholder="Introduce password..."><br>
+                            <label for="pass2">Repite password:</label><br>
+                            <input type="password" id="pass2" name="pass2" placeholder="Repite password..."><br><br>
+                            <input type="submit" value="Enviar">
+                        </div>
+                    </form>
+                </div>
+
+                <div>
+                    <h3>Formulario de Login</h3>
+                    <form id="form2" action="#" method="POST">
+                        <div>
+                            <label for="email2">Email:</label><br>
+                            <input type="email" id="email2" name="email2" placeholder="Introduce email..."><br>
+                            <label for="pass3">Password:</label><br>
+                            <input type="password" id="pass3" name="pass3" placeholder="Introduce password..."><br>
+                            <input type="submit" value="Enviar">
+                        </div>
+                    </form>
+
+                </div>
+                <div>
+                    <h3>Logout</h3>
+                    <div id="logout">
+                        <button id="salir">Logout</button>
+                    </div>
+
+                </div>
+            </div>
+        </section>`;
+}
+
+welcome();
+
+
+/* FIREBASE */
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC7ksjjVQ2TBJeBqbYKJcJZC_CmhlXf_n0",
+    authDomain: "quiz-2-d2b38.firebaseapp.com",
+    projectId: "quiz-2-d2b38",
+    storageBucket: "quiz-2-d2b38.appspot.com",
+    messagingSenderId: "946222108523",
+    appId: "1:946222108523:web:3b3f84b35c4a4767ee462e"
+};
+
+firebase.initializeApp(firebaseConfig);// Inicializaar app Firebase
+
+const db = firebase.firestore();// db representa mi BBDD //inicia Firestore
+
+
+
+//********FIRESTORE USERS COLLECTION******
+
+const createUser = (user) => {
+    db.collection("users")
+        .add(user)
+        .then((docRef) => console.log("Document written with ID: ", docRef.id))
+        .catch((error) => console.error("Error adding document: ", error));
+};
+
+/**************Firebase Auth*****************/
+
+const signUpUser = (email, password) => {
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in
+            let user = userCredential.user;
+            console.log(`se ha registrado ${user.email} ID:${user.uid}`)
+            alert(`se ha registrado ${user.email} ID:${user.uid}`)
+            // ...
+            // Saves user in firestore
+            createUser({
+                id: user.uid,
+                email: user.email,
+            });
+
+        })
+        .catch((error) => {
+            console.log("Error en el sistema" + error.message, "Error: " + error.code);
+        });
+};
+
+
+document.getElementById("form1").addEventListener("submit", function (event) {
+    event.preventDefault();
+    let email = event.target.elements.email.value;
+    let pass = event.target.elements.pass.value;
+    let pass2 = event.target.elements.pass2.value;
+
+    pass === pass2 ? signUpUser(email, pass) : alert("error password");
+})
+
+
+const signInUser = (email, password) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // Signed in
+            let user = userCredential.user;
+            console.log(`se ha logado ${user.email} ID:${user.uid}`)
+            alert(`se ha logado ${user.email} ID:${user.uid}`)
+            console.log("USER", user);
+            quiz();
+        })
+        .catch((error) => {
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            console.log(errorCode)
+            console.log(errorMessage)
+        });
+}
+
+const signOut = () => {
+    let user = firebase.auth().currentUser;
+
+    firebase.auth().signOut().then(() => {
+        console.log("Sale del sistema: " + user.email)
+        document.querySelector("#userLog").innerHTML += "Hasta la vista baby";
+    }).catch((error) => {
+        console.log("hubo un error: " + error);
+    });
+}
+
+
+document.getElementById("form2").addEventListener("submit", function (event) {
+    event.preventDefault();
+    let email = event.target.elements.email2.value;
+    let pass = event.target.elements.pass3.value;
+    signInUser(email, pass)
+})
+
+
+document.getElementById("salir").addEventListener("click", signOut);
+
+// Listener de usuario en el sistema
+// Controlar usuario logado
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log(`Está en el sistema:${user.email} ${user.uid}`);
+    } else {
+        console.log("no hay usuarios en el sistema");
+    }
+});
+
+
+/* QUIZ  */
 let partida = 0;
 let datosPartidas = [];
 
@@ -34,7 +198,8 @@ async function quiz() {
     function final() {
         document.getElementById("quiz").innerHTML = `
             <button id="again"><a href="#">Otra partida</a></button>
-            <button id="verGrafica"><a href="#">Mostrar gráfica</a></button>`
+            <button id="verGrafica"><a href="#">Mostrar gráfica</a></button>
+            <button id="signOut"><a href="#">Sign Out</a></button>`
         document.getElementById("again").addEventListener("click", function () {
             document.querySelector(".ct-chart").innerHTML = "";
             quiz();
@@ -49,12 +214,12 @@ async function quiz() {
             let datosPartidas = JSON.parse(localStorage.getItem("partidas"));
             //console.log(datosPartidas);
             let partidas = datosPartidas.map(partidas => `Partida ${partidas.partida}`);
-            
+
 
             let puntuaciones = datosPartidas.map(partidas => partidas.aciertos);
             console.log(puntuaciones);
 
-    
+
 
             new Chartist.Line('.ct-chart', {
                 labels: partidas,
@@ -68,11 +233,16 @@ async function quiz() {
                 }
             });
         });
+
+        document.getElementById("signOut").addEventListener("click", function () {
+            document.querySelector(".ct-chart").innerHTML = "";
+            welcome();
+        });
     }
 
 
     function pintar(i) {
-
+        document.getElementById("welcome").innerHTML = "";
         document.getElementById("quiz").innerHTML = `
             <fieldset>
                 <legend>${preguntas[i]}</legend>
@@ -124,9 +294,6 @@ async function quiz() {
 
                     localStorage.setItem("partidas", JSON.stringify(nuevaPartida));
 
-
-
-
                     console.log(datosPartidas);
 
                     // alert(`Has acertado ${aciertos} de 10`)
@@ -141,5 +308,3 @@ async function quiz() {
     }
     pintar(index);
 }
-
-quiz();
